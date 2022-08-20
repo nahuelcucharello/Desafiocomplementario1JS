7c1442c9f79b0627be760652e4371bebc7be0fbb
nahuelcucharello <106846736+nahuelcucharello@users.noreply.github.com>
2022-08-19 22:27:24 -0300

    Desafío complementario 1

---
+++ index.js
@@ -0,0 +1,7 @@
+let palabra = prompt ('Ingrese la palabra correcta')
+while(palabra!= 'hola'){
+    alert('La palabra ingresada es válida')
+    palabra = prompt ('Ingrese la palabra correcta')
+}
+alert("La palabra ingresada no es válida")
+palabra = prompt ('Ingrese la palabra correcta') 
